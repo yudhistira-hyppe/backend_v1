385d0ea8e782f979721f4327ab2584c30a531652
yd-tak <yudhistiradewanata@teknologiabadikorpora.com>
2024-09-04 12:58:34 +0700

    update wd log

--- src/paymentgateway/oypg/oypg.service.ts
+++ src/paymentgateway/oypg/oypg.service.ts
@@ -11,6 +11,7 @@ export class OyPgService {
     async inquiryAccount(accountInfo: OyAccountInquiry): Promise<OyAccountInquiryResponse> {
         let config = { headers: { "x-oy-username": this.configService.get("OY_USERNAME"), "x-api-key": this.configService.get("OY_APIKEY") } };
         const res = await this.httpService.post(this.configService.get("OY_ENDPOINT") + 'account-inquiry', accountInfo, config).toPromise();
+        console.log(res);
         const data = res.data;
         return data;
     }
@@ -18,6 +19,7 @@ export class OyPgService {
     async disbursement(disbursement: OyDisbursement): Promise<OyDisbursementResponse> {
         let config = { headers: { "x-oy-username": this.configService.get("OY_USERNAME"), "x-api-key": this.configService.get("OY_APIKEY") } };
         const res = await this.httpService.post(this.configService.get("OY_ENDPOINT") + 'remit', disbursement, config).toPromise();
+        console.log(res);
         const data = res.data;
         return data;
     }
@@ -25,6 +27,7 @@ export class OyPgService {
     async disbursementStatus(disbursementStatus: OyDisbursementStatus): Promise<OyDisbursementStatusResponse> {
         let config = { headers: { "x-oy-username": this.configService.get("OY_USERNAME"), "x-api-key": this.configService.get("OY_APIKEY") } };
         const res = await this.httpService.post(this.configService.get("OY_ENDPOINT") + 'remit-status', disbursementStatus, config).toPromise();
+        console.log(res);
         const data = res.data;
         return data;
     }
@@ -32,6 +35,7 @@ export class OyPgService {
     async myBalance(): Promise<OyMyBalanceResponse> {
         let config = { headers: { "x-oy-username": this.configService.get("OY_USERNAME"), "x-api-key": this.configService.get("OY_APIKEY") } };
         const res = await this.httpService.get(this.configService.get("OY_ENDPOINT") + 'balance', config).toPromise();
+        console.log(res);
         const data = res.data;
         return data;
     }
@@ -39,6 +43,7 @@ export class OyPgService {
     async generateStaticVa(staticVa: OyStaticVa): Promise<OyStaticVaResponse> {
         let config = { headers: { "x-oy-username": this.configService.get("OY_USERNAME"), "x-api-key": this.configService.get("OY_APIKEY") } };
         const res = await this.httpService.post(this.configService.get("OY_ENDPOINT") + 'generate-static-va', staticVa, config).toPromise();
+        console.log(res);
         const data = res.data;
         return data;
     }
@@ -46,6 +51,7 @@ export class OyPgService {
     async staticVaInfo(no: string): Promise<OyStaticVAInfo> {
         let config = { headers: { "x-oy-username": this.configService.get("OY_USERNAME"), "x-api-key": this.configService.get("OY_APIKEY") } };
         const res = await this.httpService.get(this.configService.get("OY_ENDPOINT") + 'static-virtual-account/' + no, config).toPromise();
+        console.log(res);
         const data = res.data;
         return data;
     }
